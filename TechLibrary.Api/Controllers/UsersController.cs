7c016c43234c54ea1a2c9ac205de810787decae4
Franklin Marden <rodrigues_marden@hotmail.com>
2025-02-27 06:01:59 -0300

    Api project init

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Created();
+    }
+}
