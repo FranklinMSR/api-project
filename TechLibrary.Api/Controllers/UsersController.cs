3d382ed12ef5a693613888175150908be4c9e819
Franklin Marden <rodrigues_marden@hotmail.com>
2025-02-27 10:31:51 -0300

    Api maker and modeling db

--- TechLibrary.Api/Controllers/UsersController.cs
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.UseCases.Users.Register;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
 
 namespace TechLibrary.Api.Controllers;
 
@@ -7,8 +11,31 @@ namespace TechLibrary.Api.Controllers;
 public class UsersController : ControllerBase
 {
     [HttpPost]
-    public IActionResult Create()
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
+    public IActionResult Create(RequestUserJson request)
     {
-        return Created();
+        try
+        {
+            var useCase = new RegisterUserUseCase();
+
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        catch (TechLibraryException ex)
+        {
+            return BadRequest(new ResponseErrorMessagesJson
+            {
+                Errors = ex.GetErrorMessages()
+            });
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorMessagesJson
+            {
+               Errors = ["Erro Desconhecido"]
+            });
+        }
     }
 }
